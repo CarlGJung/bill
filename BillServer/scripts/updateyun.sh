45f15261eac725fe16cdbcf2a2a0c42469a47023
wangchao <wangchao@bjtxra.com>
2018-09-02 21:19:05 +0800

    部署到阿里云 #10

--- BillServer/scripts/updateyun.sh
+++ BillServer/scripts/updateyun.sh
@@ -2,8 +2,8 @@
 scp ./bill.service root@bill.carl-jung.top:/opt/bill
 scp ./install-update.sh root@bill.carl-jung.top:/opt/bill
 scp -r ../target/lib root@bill.carl-jung.top:/opt/bill
-scp ../config/config.json.tmpl root@bill.carl-jung.top:/opt/bill
-scp ../config/logback.xml.tmpl root@bill.carl-jung.top:/opt/bill
+scp ../config/config.json.tmpl root@bill.carl-jung.top:/opt/bill/config
+scp ../config/logback.xml.tmpl root@bill.carl-jung.top:/opt/bill/config
 scp ./start.sh root@bill.carl-jung.top:/opt/bill
 scp ../target/Bill-1.0-SNAPSHOT.jar root@bill.carl-jung.top:/opt/bill
 scp -r ../../bill-react/build root@bill.carl-jung.top:/opt/bill
